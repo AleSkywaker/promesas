7d817f18e01872e73cb584b743714193213a4406
Alex Colombo <uricaine@hotmail.com>
2019-02-17 11:51:06 +0100

    fetch

--- js/fetch/fetch.js
+++ js/fetch/fetch.js
@@ -1,20 +1,20 @@
 const url = 'https://reqres.in/api/users';
 
-// fetch(url)
-//     .then((resp) => resp.json())
-//     .then((respObj) => {
-//         console.log(respObj.data[0].last_name)
-//         console.log(respObj.page)
-//     })
+fetch(url)
+    .then((resp) => resp.json())
+    .then((respObj) => {
+        console.log(respObj.data[0].last_name)
+        console.log(respObj.page)
+    })
 
 
-    const url2 = "https://www.wikipedia.org/"
+    // const url2 = "https://www.wikipedia.org/"
 
-    fetch(url2)
-        .then(resp =>resp.text())
-        .then((h)=>{
-            console.log("pasa por aqui" + h)
-            document.open();
-            document.write(html);
-            document.close();
-        })
+    // fetch(url2)
+    //     .then(resp =>resp.text())
+    //     .then((h)=>{
+    //         console.log("pasa por aqui" + h)
+    //         document.open();
+    //         document.write(html);
+    //         document.close();
+    //     })
