ff467ed375650c40248de34c3450220d24ecc084
Alex Colombo <uricaine@hotmail.com>
2019-02-17 11:37:02 +0100

    fetch

---
+++ js/fetch/fetch.js
@@ -0,0 +1,17 @@
+const url = 'https://reqres.in/api/users';
+
+// fetch(url)
+//     .then((resp) => resp.json())
+//     .then((respObj) => {
+//         console.log(respObj.data[0].last_name)
+//         console.log(respObj.page)
+//     })
+
+
+    fetch('https://www.meneame.net/')
+        .then(resp =>resp.json())
+        .then(html=>{
+            document.open();
+            document.write(html);
+            document.close();
+        })
